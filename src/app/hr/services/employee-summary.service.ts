cc6b03be0c25d9e322530a38c1e4c87a47a788ec
saifeddin1 <saifeddinmatoui4@gmail.com>
2022-02-15 16:36:03 +0100

    use token from LMS

--- src/app/hr/services/employee-summary.service.ts
+++ src/app/hr/services/employee-summary.service.ts
@@ -6,13 +6,18 @@ import { Contract } from '../models/contract.model';
 import { File } from '../models/file.models';
 import { Interview } from '../models/interview.model';
 import { Timesheet } from '../models/timesheet.model';
+import { Timeslot } from '../models/timeslot.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class EmployeeSummaryService {
+  // public decodedToken = jwtDecode(
+  //   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjFmYWE0ZGY3OWFlZTc4MWNhNmVlOTZhIiwicm9sZSI6IlNUVURFTlQifSwiaWF0IjoxNjQzODE2NTc4fQ.uxmx9-nyNJgK5PoumH_s0UqVRlLBLHeQJXQWRbJQJ7k'
+  // );
+
   public decodedToken = jwtDecode(
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjFmYWE0ZGY3OWFlZTc4MWNhNmVlOTZhIiwicm9sZSI6IlNUVURFTlQifSwiaWF0IjoxNjQzODE2NTc4fQ.uxmx9-nyNJgK5PoumH_s0UqVRlLBLHeQJXQWRbJQJ7k'
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjcmVkaXQiOjAsImNoYXRzIjpbXSwiX2lkIjoiNjIwMGYwNTU1ZTBlOWI5ZWFjM2YyMmFlIiwidXNlcm5hbWUiOiJzYWlmZWRkaW4wMSIsImVtYWlsIjoic2FpZjFAZ21haWwuY29tIiwicHJvZmlsZSI6eyJmdWxsTmFtZSI6IlNhaWZlZGRpbiBNYXRvdWkiLCJwaG9uZSI6IjEyMzQ1Njc4IiwibGlua2VkSW4iOiJzYWlmZWRkaW4wMSIsImZhY2Vib29rIjoiIn0sInR5cGUiOiJFU1RVREVOVCIsInN0dWRlbnROaXZlYXVJZCI6IjYxMDgyYTlkNmM1MzYwMmIxYzYwYzhhZiIsImNyZWF0ZWRBdCI6IjIwMjItMDItMDdUMTA6MTE6MzMuMjMxWiIsInVwZGF0ZWRBdCI6IjIwMjItMDItMTVUMDg6NDg6NDIuNzAxWiIsIl9fdiI6MCwiaWF0IjoxNjQ0OTE2MjI4LCJleHAiOjE2NjI5MTYyMjh9.0tPzDABqmcNXw8frdKF5dgkdyz6It_PAOkoRZgKpUU0'
   );
 
   private BASE_URL: string = 'http://localhost:8001';
@@ -41,7 +46,8 @@ export class EmployeeSummaryService {
   }
 
   getUser() {
-    return this.decodedToken['user'];
+    return this.decodedToken;
+    // return this.decodedToken['user'];
   }
   getFiles(): Observable<File[]> {
     return this.http.get<File[]>(`${this.BASE_URL}/files`);
@@ -63,5 +69,9 @@ export class EmployeeSummaryService {
       JSON.stringify(body),
       { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) }
     );
+  }
+
+  getTimeSlots(): Observable<Timeslot[]> {
+    return this.http.get<Timeslot[]>(`${this.BASE_URL}/timeslots`);
   }
 }
