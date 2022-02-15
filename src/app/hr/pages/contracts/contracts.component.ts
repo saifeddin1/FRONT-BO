84ae0a4408f58577db9cf4b0a14602f68935e806
saifeddin1 <saifeddinmatoui4@gmail.com>
2022-02-15 16:36:48 +0100

    Upadted contracts history tab

---
+++ src/app/hr/pages/contracts/contracts.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { Contract } from '../../models/contract.model';
+import { EmployeeSummaryService } from '../../services/employee-summary.service';
+import { formatDate } from '../../helpers/formatDate';
+
+@Component({
+  selector: 'app-contracts',
+  templateUrl: './contracts.component.html',
+  styleUrls: ['./contracts.component.css'],
+})
+export class ContractsComponent implements OnInit {
+  public contracts: Contract[];
+  constructor(private summaryService: EmployeeSummaryService) {}
+
+  public isDisabled: boolean =
+    this.summaryService.getUser()['type'] !== 'ADMIN';
+
+  ngOnInit(): void {
+    this.getContracts();
+  }
+
+  formatedDate(date) {
+    return formatDate(date);
+  }
+
+  getContracts() {
+    this.summaryService.getContractsWithSalary().subscribe((result) => {
+      this.contracts = result['response'];
+      console.log(
+        '⚡  this.summaryService.getContractsWithSalary ~ this.contracts',
+        this.contracts
+      );
+    });
+  }
+}
