d3dc7210efbef0f3f64388b4510cee877f14117b
yelouni <yelouni@naxxum.fr>
2022-03-21 14:54:49 +0100

    mup-app

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/uimpactify-app'),
+      dir: require('path').join(__dirname, './coverage/mup-app'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
