33e534b878c82da332b1bc765e588529d768246b
Saifeddin Matoui <me.saifeddin@gmail.com>
2022-03-23 12:20:00 +0000

    Updated environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: false,
     LmsApiUrl: 'http://localhost:5000',
     IdentityApi: 'http://localhost:2022',
-    HRApi: 'http://localhost:8001',
+    HRApi: 'http://localhost:8080',
     Accounting:'http://localhost:2044',  
 };
