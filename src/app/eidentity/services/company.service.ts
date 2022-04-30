a83477c2bae5e3db1d03d02301510cbbfb881554
Souissi Ahmed Aziz <ahmedazizsouissi@gmail.com>
2022-04-30 03:33:18 +0100

    animation dep number

--- src/app/eidentity/services/company.service.ts
+++ src/app/eidentity/services/company.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,23 +10,23 @@ export class CompanyService {
 
   constructor(private http:HttpClient) { }
 
-  getCompanies(){
-        return this.http.get(environment.IdentityApi + '/api/v1/company/getCompanies')
+  getCompanies():Observable<any>{
+        return this.http.get<any>(environment.IdentityApi + '/api/v1/company/getCompanies')
     }
 
-  addCompany(data:any){
-      return this.http.post(environment.IdentityApi +"/api/v1/company/addCompany" ,data)
+  addCompany(data:any):Observable<any>{
+      return this.http.post<any>(environment.IdentityApi +"/api/v1/company/addCompany" ,data)
   }
 
-  deleteCompany(id:any){
-    return this.http.delete(environment.IdentityApi + "/api/v1/company/deleteCampany/" + id)
+  deleteCompany(id:any):Observable<any>{
+    return this.http.delete<any>(environment.IdentityApi + "/api/v1/company/deleteCampany/" + id)
   }
 
-  getOneById(id:any){
-    return this.http.get(environment.IdentityApi +"/api/v1/company/getCompanybyId/" + id)
+  getOneById(id:any):Observable<any>{
+    return this.http.get<any>(environment.IdentityApi +"/api/v1/company/getCompanybyId/" + id)
   }
-  geByowner(id:any){
-    return this.http.get(environment.IdentityApi +"/api/v1/company/companybyowner/" + id)
+  geByowner(id:any):Observable<any>{
+    return this.http.get<any>(environment.IdentityApi +"/api/v1/company/companybyowner/" + id)
   }
 
 }
