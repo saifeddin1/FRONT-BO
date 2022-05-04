c89585f1c2268bbffacbafbe07eafd6202c31cfe
saifeddin1 <saifeddinmatoui4@gmail.com>
2022-05-04 20:47:07 +0000

    All add buttons are disabled if not fields filled

--- src/app/lms/pages/user-profile/user-profile.component.ts
+++ src/app/lms/pages/user-profile/user-profile.component.ts
@@ -15,26 +15,25 @@ export class UserProfileComponent implements OnInit {
   user: any;
   image: string;
   fullname: string;
-  phone:string;
-  address: string;
+  phone: string;
+  // address: string;
   email: string;
-  description: string;
+  // description: string;
   err: string; // `backend/routes/user.js`
   sameUser: boolean;
   username: string;
   loading: boolean = true;
   updateduser = {
-      username: '',
-      email: '',
-      phone: '',
-      address :'',
-      description : ''
-     }
+    username: '',
+    email: '',
+    phone: '',
+    // address: '',
+    // description : ''
+  };
 
-  
   constructor(
     private toasterService: ToasterService,
-    private usersServiceidentity:UsersService,
+    private usersServiceidentity: UsersService,
     private userService: UserService,
     private route: ActivatedRoute,
     private router: Router
@@ -57,38 +56,27 @@ export class UserProfileComponent implements OnInit {
   initComponent(): void {
     this.username = this.route.snapshot.paramMap.get('username');
     this.user = this.userService.getCurrentUser();
-    
-    
 
-    this.usersServiceidentity.getOneUser(this.user._id).subscribe(
-      (res)=>{
-        console.log("******* req *******", res.response)
-        this.updateduser.username = res.response.username;
-        this.updateduser.email = res.response.email;
-        this.updateduser.phone = res.response.phone;
-        this.updateduser.address = res.response.address;
-        this.updateduser.description = res.response.description
+    this.usersServiceidentity.getOneUser(this.user._id).subscribe((res) => {
+      console.log('******* req *******', res.response);
+      this.updateduser.username = res.response.username;
+      this.updateduser.email = res.response.email;
+      this.updateduser.phone = res.response.phone;
+      // this.updateduser.address = res.response.address;
+      // this.updateduser.description = res.response.description
+    });
+  }
 
-
-      },
-     
-    )
-  
+  updateuser() {
+    this.usersServiceidentity
+      .updateUser(this.user._id, this.updateduser)
+      .subscribe(
+        (res) => {
+          this.toasterService.success('upadted successfully');
+        },
+        (err) => {
+          this.toasterService.error('something wrong when updating');
+        }
+      );
+  }
 }
-
-updateuser(){
- 
-
- this.usersServiceidentity.updateUser(this.user._id,this.updateduser).subscribe(
-   (res)=>{
-     this.toasterService.success("upadted successfully")
-
-   },
-   (err)=>{
-    this.toasterService.error("something wrong when updating")
-   }
- )
-}
-
-}
-
